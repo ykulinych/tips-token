3b2c8190458dda9ded8912ed05202d86c4436e2a
Yan Kulinych <ykulinych@gmail.com>
2025-02-14 13:21:50 +0000

    tips-token program

--- programs/tips-token/src/lib.rs
+++ programs/tips-token/src/lib.rs
@@ -1,16 +1,51 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{self, MintTo, Token, TokenAccount};
 
 declare_id!("ApvtKKB1ZgfSB79jezCNossC7H3faQJKizVTLGsx2JxP");
 
 #[program]
-pub mod tips_token {
+pub mod tipstoken {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    // Метод для аирдропа токенов
+    pub fn airdrop(ctx: Context<Airdrop>, amount: u64) -> Result<()> {
+        let cpi_accounts = MintTo {
+            mint: ctx.accounts.mint.to_account_info(),
+            to: ctx.accounts.token_account.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+        token::mint_to(cpi_ctx, amount)
+    }
+
+    // Метод для перевода токенов
+    pub fn transfer(ctx: Context<Transfer>, amount: u64) -> Result<()> {
+        let cpi_accounts = token::Transfer {
+            from: ctx.accounts.from.to_account_info(),
+            to: ctx.accounts.to.to_account_info(),
+            authority: ctx.accounts.authority.to_account_info(),
+        };
+        let cpi_ctx = CpiContext::new(ctx.accounts.token_program.to_account_info(), cpi_accounts);
+        token::transfer(cpi_ctx, amount)
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Airdrop<'info> {
+    #[account(mut)]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub token_account: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+}
+
+#[derive(Accounts)]
+pub struct Transfer<'info> {
+    #[account(mut)]
+    pub from: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub to: Account<'info, TokenAccount>,
+    pub authority: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+}
